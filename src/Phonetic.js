b4775bbc57578b236ebef09e87003256c51a3702
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-05-10 14:59:04 -0400

    Updated phonetics to play sound in page with ReactAudioPlayer
    Updated phonetics to play sound in page with ReactAudioPlayer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,17 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function Phonetic(props){
+
+function playMusic(){
+let player = document.getElementById('player');
+player.play();
+}
 if (props.phonetic !== null){
 return(
 <div className="Phonetic">
-    <a href={props.phonetic.audio} target="_blank"><i className="fas fa-volume-up"></i></a> &nbsp;
+    <ReactAudioPlayer src={props.phonetic.audio} id="player" />
+        <i className="fas fa-volume-up" onClick={playMusic}></i> &nbsp;
     {props.phonetic.text}
 </div>)}
 else return null;
