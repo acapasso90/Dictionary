c5dfd2abdb240ce29be44319f7f82b6e74c2dcff
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-05-10 19:10:17 -0400

    Added images integration via Photos.js
    Added images integration via Photos.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,28 +1,30 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(){
 
-let [keyword, setKeyword] = useState("Persnickety"); 
+let [keyword, setKeyword] = useState("Strawberry"); 
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [photos, setPhotos] = useState(null);
 
 function handleResponse(response){
     setResults(response.data[0]);
 }
 
 function handlePexelsResponse(response){
-    console.log(response);
+    setPhotos(response.data.photos);
 }
+
 function search(){ 
  //api documentation https://dictionaryapi.dev/
  let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
  axios.get(apiURL).then(handleResponse);
 const pexelsApiKey = '563492ad6f91700001000001c20edfc6c7e14112a4ba15789c52d868';
-let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-axios
-.get(pexelsApiUrl, {
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+axios.get(pexelsApiUrl, {
     headers: { Authorization : `Bearer ${pexelsApiKey}` },
 })
 .then(handlePexelsResponse);
@@ -50,6 +52,7 @@ if (loaded){
                 <button onClick={search}><i className="fas fa-search"></i></button>
             </form>
             <Results data={results} />
+            <Photos photos={photos} />
         </div>);
 }
 
---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Photos(props){
+    console.log(props.photos);
+if (props.photos){
+    return(
+        <div className="Photos">
+            <div className="row">
+                {props.photos.map(function(photo, index) {
+                    return (
+                        <div className="col-4" key={index}>
+                        <a href={photo.src.original} target="_blank">
+                        <img src={photo.src.landscape}  className="img-fluid" />
+                        </a>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+else return null;
+}
