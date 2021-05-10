fd38de3d0356bbabc24dc5311af370b3386e3f0d
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-05-10 13:14:36 -0400

    Added meaning and results.js to display multiple definitions
    Added meaning and results.js to display multiple definitions

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Result(props){
     console.log(props.data)
-return(<div className="result">
-    Results!
+    if (props.data != null){ return(
+        <div className="result">
+            <h2>{props.data.word}</h2>
+            {props.data.meanings.map(function(meaning, index) {
+               return(<div key={index}>
+                   <Meaning meaning={meaning} />
+                   </div>)
+            })}
         </div>)
+    }
+    else {return null;}
 }
