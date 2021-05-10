a46ca2ff671a515cc63b42078634949c5fd5c745
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-05-10 11:09:07 -0400

    Set up results and send api response as props from Dictionary.js
    Set up results and send api response as props from Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,23 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary(){
 
 let [keyword, setKeyword] = useState(""); 
+let [results, setResults] = useState("");
+
 
 function handleResponse(response){
-    console.log(response.data);
+    setResults(response.data[0]);
+   // let firstDef = response.data[0].meanings[0].definitions[0].definition;
+   // if (response.data[0].meanings.length > 1) { setSecondDef(response.data[0].meanings[1].definitions[0].definition)}
+   // console.log(secondDef)
 }
 
 function search(event){
     event.preventDefault();
+    //api documentation https://dictionaryapi.dev/
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
     axios.get(apiURL).then(handleResponse);
 }
@@ -24,6 +31,7 @@ return(
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} autoFocus={true} autoComplete="off" />
         </form>
+        <Results data={results} />
     </div>);
 
 }
