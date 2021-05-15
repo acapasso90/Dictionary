94cbbc2f475efd920de3bb3359c86d223d3fa513
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-05-15 10:45:54 -0400

    Updated Meaning to not show part of speech if undefined (foreign languages)
    Updated Meaning to not show part of speech if undefined (foreign languages)

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Synonyms from "./Synonyms";
 import Example from "./Example";
 
 export default function Meaning(props){
-    return(
+    const [partofSpeech, setPartofSpeech] = useState(props.meaning.partOfSpeech)
+    if (partofSpeech === undefined){  setPartofSpeech(null);}
+
+
+    useEffect(()=>{ if (partofSpeech === "undefined"){  setPartofSpeech(null);}
+    else{ setPartofSpeech(props.meaning.partOfSpeech)};}, [props.meaning.partOfSpeech]);
+    
+    console.log(partofSpeech)
+ return(
 <div className="Meaning">
-        <h3 className="purple" id="partOfSpeech"> {props.meaning.partOfSpeech} </h3>
+        <h3 className="purple" id="partOfSpeech"> {partofSpeech} </h3>
         {props.meaning.definitions.map(function(definition, index) {
             let formattedIndex = ++index
             return (
